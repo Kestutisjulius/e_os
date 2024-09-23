56739386effca21006746f5e5197509000c44d54
Kęstutis Skrebė <kestutis.julius@gmail.com>
2024-09-23 13:39:55 +0300

    first commit

---
+++ src/main/java/eu/minted/eos/EverythingOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package eu.minted.eos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverythingOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverythingOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/minted/eos/EverythingOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.minted.eos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverythingOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
